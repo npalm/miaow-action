193587f5c975074ef85f29324e23d930691983a6
Niek Palm <niek.palm@philips.com>
2024-04-03 12:24:38 +0200

    chore: move to esm

---
+++ dist/cat.d.ts
@@ -0,0 +1,1 @@
+export declare function getRandomCatImage(): Promise<string>;
---
+++ dist/github.d.ts
@@ -0,0 +1,2 @@
+export declare function findMiauw(owner: string, repo: string, issue: number, token: string): Promise<number | undefined>;
+export declare function insertMiaow(owner: string, repo: string, issue: number, token: string, catImageUrl: string, commentId?: number): Promise<void>;
